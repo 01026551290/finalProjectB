70df5c1a9eab57ee55645392333bc62f94060a60
user1 <user1@DESKTOP-8UC660Q>
2019-07-16 21:31:22 +0900

    20190716/21:31/마진수 오늘 한것 그래프 보이기까지

---
+++ FinalProjectB/src/main/java/com/spring/god/jinsoo/model/JinsooDAO.java
@@ -0,0 +1,8 @@
+package com.spring.god.jinsoo.model;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JinsooDAO {
+
+}
---
+++ FinalProjectB/src/main/java/com/spring/god/jinsoo/service/InterJinsooService.java
@@ -0,0 +1,5 @@
+package com.spring.god.jinsoo.service;
+
+public interface InterJinsooService {
+
+}
