8074b9691ff9047af719d45e9e74a601f6e1cd23
SAMSUNG <SAMSUNG@DESKTOP-ACSVAO6>
2019-08-07 21:43:43 +0900

    190807/정혜인/이미지 다중 파일 추가 수정/21:43

--- FinalProjectB/src/main/java/com/spring/god/hyein/service/InterAdminService.java
+++ FinalProjectB/src/main/java/com/spring/god/hyein/service/InterAdminService.java
@@ -1,5 +1,6 @@
 package com.spring.god.hyein.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.spring.god.hyein.model.HotelRoomVO;
@@ -13,12 +14,23 @@ public interface InterAdminService {
 	// 객실 등록의 숙소명 검색어 자동완성하기
 	List<String> wordNameSearchView(String searchWord);
 
-	// 룸 등록하기
-	int roomAdd(HotelRoomVO hotelroomvo);
 
 	// 자동완성 검색어의 대분류 카테고리 넘버 가져오기
 	String getLargeCodeNum(String clickWord);
+	// 자동완성 검색어의 대분류 카테고리 넘버 가져오기2
+	String getLargeCategoryNum(String clickWord);
 
-	int photoaddimg(List<PhotoVO> photovo);
+//	int photoaddimg(List<PhotoVO> photovo);
 	
+	// 객실 등록하기
+	int roomAdd(HashMap<String, String> productMap);
+
+	// 제품일련번호 체번해오기
+	int getProdseq(HashMap<String, String> hashMap);
+
+	// 객실 이미지 넣기
+	int imgAdd(HashMap<String, String> hashMap);
+
+	
+
 }
